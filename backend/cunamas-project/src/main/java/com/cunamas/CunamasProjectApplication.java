5ef2d1d5d454ae254fa9d1a5a14b5195a2f15596
saidhhgomez <saidhuallancagomez@gmail.com>
2026-05-23 22:21:57 -0500

    estructura inicial

---
+++ backend/cunamas-project/src/main/java/com/cunamas/CunamasProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cunamas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CunamasProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CunamasProjectApplication.class, args);
+	}
+
+}
---
+++ backend/cunamas-project/src/test/java/com/cunamas/CunamasProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cunamas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CunamasProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
